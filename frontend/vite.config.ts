1dc0fc05ced84706c62ae274e60f3fd6075e4ac8
pmarceaujr <paul@themarceaus.net>
2026-01-28 15:34:24 -0500

    Updates for Heroku deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,16 @@
 import react from "@vitejs/plugin-react";
-import path from "path";
+// import path from "path";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
+    plugins: [react(), tsconfigPaths()],
+    // resolve: {
+    //     alias: {
+    //         "@": path.resolve(__dirname, "./src"),
+    //     },
+    // },
     server: {
         port: 3000,
         proxy: {
