49b65b3d276518dcf399bafd5a7edbbb6b8948cc
pmarceaujr <paul@themarceaus.net>
2026-01-28 14:00:47 -0500

    Updates for Heroku deploy

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -87,6 +87,8 @@ def create_app(config_name='development'):
         # Heroku provides "postgres://" but SQLAlchemy wants "postgresql://"
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
     
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL 
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
     migrate.init_app(app, db)
